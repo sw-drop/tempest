642e02d087576e134168fe72d783eb6855a31d04
sw-drop <gary@pillay.net>
2026-07-16 15:29:17 +0100

    Fix push_v6.sh to include .env and use correct ssh alias

--- sfro-dash-v6/push_v6.sh
+++ sfro-dash-v6/push_v6.sh
@@ -7,11 +7,12 @@ REMOTE_HOST="Pi5-1"
 # (or v6 if it gets updated later). The user said: "The deployment of the dashboard is currently at /docker/sfro-dash-v5. As part of this project, we may change that to /docker/sfro-dash-v6"
 REMOTE_PATH="/docker/sfro-dash-v5/"
 LOCAL_PATH="./deployed_container/"
+REMOTE_HOST="Pi5-1"
 
 echo "Pushing full V6 update to ${REMOTE_HOST}:${REMOTE_PATH}..."
 
-# We use rsync to sync the entire container context and omit caches/secrets
-rsync -avz --exclude="*.pyc" --exclude="__pycache__" --exclude=".env" --exclude=".git" "$LOCAL_PATH" "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}"
+# We use rsync to sync the entire container context and omit caches/git
+rsync -avz --exclude="*.pyc" --exclude="__pycache__" --exclude=".git" "$LOCAL_PATH" "${REMOTE_HOST}:${REMOTE_PATH}"
 
 echo "Done! To apply these changes on the server, you will need to rebuild the container:"
 echo "  docker compose up --build -d"
